1552cd6771ada19c9f06c74ace4a4a3fc7f27fe1
Viking <wiktor.przadka.1@gmail.com>
2020-02-16 00:19:43 +0100

    add contours detection

--- recognieze.py
+++ recognieze.py
@@ -11,7 +11,20 @@ if __name__ == '__main__':
 
         while runFlag:
             status, img = capture.read()
+            grayScale = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            blue, green, red = cv2.split(img)
+
+            edgeDet = cv2.Canny(img, 100, 200)
+            status, mask = cv2.threshold(grayScale, 100, 200, 0)
+            # imgAnd = cv2.bitwise_and(img, img, mask=mask)
+
+            contour, hierarchy = cv2.findContours(edgeDet, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+            cv2.drawContours(img, contour, -1, (0, 255, 0), 3)
             cv2.imshow('webcam', img)
+            cv2.imshow('mask', mask)
+            # cv2.imshow('blue', blue)
+            cv2.imshow('edgeDet', edgeDet)
 
             if cv2.waitKey(16) & 0xFF == ord('q'):
                 runFlag = False
